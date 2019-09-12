130e15eb9cd1298fa7d8768435cc90e5497b0f6c
YongZhong <373605178@qq.com>
2019-09-12 18:28:34 +0800

    2019-09-11

--- Apollo_issue.cpp
+++ Apollo_issue.cpp
@@ -1923,6 +1923,68 @@ I will create a fix
 
 
 // 
+// 请问lattice_planner里面的Auto Tuning是用来干什么的? #5730
+Auto Tuning部分的目的是通过机器学习的方式来给代价函数的参数调优，现在相关工作还在开发之中。
+这种学习可以理解为根据场景自动为那六个代价函数分配权值吗？
+按照我的理解，Auto-tuning是学习如何根据场景来评估规划出来的trajectory，从中选择出一条最优的。它并不是去学习如何分配权重，而是学习如何给出一个cost
+
+
+
+// Question about planning and control. #4992
+Hello,
+I have a question about trajectory points in planning and control.
+
+In planning, the points in trajectory is the center of rear axle of vehicle.
+In control, the points used in MPC controller is the centroid of vehicle.
+Can they work well to follow the path?
+
+Thanks.
+
+When a vehicle deviates from the planned trajectory, usually a heading error (rather than a lateral error) appears first, causing a forward-moving vehicle to veer off a predicted course, which translates to a lateral error. In other words, lateral errors lag heading errors. By moving the controller origin forward, lateral errors appear sooner upon vehicle deviation, which improves controller stability.
+
+Planning, in theory, does not need to know where a controller thinks the vehicle origin is. All it cares is that the vehicle is able to stay on a planned trajectory with small predictable deviations.
+
+
+
+
+
+// how do you define smoothness cost in trajectory.cc #5017
+
+Hi,
+
+I'm reading apollo's code for the planning module, however got stuck on figuring out how you properly define the trajectory cost, especially smoothness cost. could you share some academic paper to refer to?
+
+Thanks so much for your help, really appreciate it.
+
+Best
+
+Here are two books I have learned, one is "Learning with Kernels: Support Vector Machines, Regularization, Optimization, and Beyond", the other is "kernel methods for pattern analysis".
+
+Hope this would help you:)
+
+
+
+
+// Trajectory stitching, why? #5033
+
+Hi Team Apollo,
+
+Can you please explain what is the purpose of stitching trajectories in planning?
+Is stitching between lastly published trajectory and something else?
+Thank you.
+
+You dont want the car change trajectory every time planner run -> car will change direction frequently
+So you keep a portion of previous plan trajectory and stitch it to new trajectory.
+
+Trajectory is much longer than the length of the ego car.
+
+
+
+// 
+
+source /apollo/scripts/apollo_base.sh
+
+cyber_launch start /apollo/modules/dreamview/launch/dreamview.launch
 
 
 
@@ -1930,8 +1992,141 @@ I will create a fix
 
 
 
+1. 
+
+  RTK Replay planning mode with additional planning tasks and driving scenarios
+  It is it possible to configure and run planning module in RTK Replay mode with ability to change line or emergency stop to avoid obstacles collision?
+  No, you need use standard mode for those functions.
 
 
+
+2.
+
+  Hello,
+
+  The following code confuses me, anyone can explain its grammar？
+
+  []() -> LocalizationBase* { return new RTKLocalization(); });
+
+  []()-> means what?
+  It is in Localization.cc of apollo1.0 and line 38.
+
+  thanks.
+
+   @krishnatoshniwal
+   
+  krishnatoshniwal commented on May 6
+  Its a lambda function with no input parameter and the return object having the type LocalizationBase*. Look up on lambda expressions here
+  https://en.cppreference.com/w/cpp/language/lambda
+
+
+
+3.
+
+    Steps to reproduce the issue:
+    Please use bullet points and include as much details as possible:
+    bash ./docker/scripts/dev_start.sh
+    bash ./docker/scripts/dev_into.sh
+    bash ./apollo.sh build
+    bash scripts/bootstrap.sh
+    Tasks: Sim Control: ON
+    Module Controller: Routing: ON
+    Module Controller: Planning: ON
+    Default Routing Sunnyvale Loop: ON
+    Gives me the screen shot below. Planning automatically OFF.
+
+      
+
+4.
+  // canbus component does not exit gracefully #8053
+
+Describe the bug
+As subject
+
+To Reproduce
+Steps to reproduce the behavior:
+
+Launch canbus component using mainboard
+>>mainboard -d /apollo/modules/canbus/dag/canbus.dag
+Shutdown it using Ctrl+C
+The console shows:
+terminate called without an active exception
+Abort 
+Cause
+No cleanup of those handles: vehicle_controller_, can_receiver_, can_sender_, and can_client_.
+
+Fix
+The cyber component has a cleanup hook function called Clear() which will be called inside ComponentBase::Shutdown(). Override of this Clear() function inside CanbusComponent fixes the issue.
+
+void CanbusComponent::Clear() {
+
+  can_sender_.Stop();
+  can_receiver_.Stop();
+  can_client_->Stop();
+  vehicle_controller_->Stop();
+
+  AINFO << "Cleanup Canbus component";
+}
+
+
+
+5. 
+
+There are three relative map mode in apollo 2.5:
+Mode 1: perception
+Mode 2: navigator + perception
+Mode 3: HDMap + navigator + perception.
+
+Does apollo 3.5 also support these modes?
+
+Those modes are not supported in 3.5 yet.
+
+
+
+6. // How can I test the control module without a real car?
+
+
+Apollo version:3.5
+I have been trying to test the performance of the sample vehicle lincoln mx8 under a given complex path. After I compiled control_component_test.cc in apollo/modules/control and manually input a straight path which started at (0,0,0) and ended at (4.6,0,0), I found the .INFO file like this:
+
+Log file created at: 2019/03/25 13:34:28
+Running on machine: in_dev_docker
+Log line format: [IWEF]mmdd hh:mm:ss.uuuuuu threadid file:line] msg
+I0325 13:34:28.812129 11484 control_component.cc:42] Control init, starting ...
+I0325 13:34:28.837960 11484 control_component.cc:48] Conf file: /apollo/modules/control/testdata/conf/control_conf.pb.txt is loaded.
+I0325 13:34:28.837970 11484 control_component.cc:50] Conf file: is loaded.
+I0325 13:34:28.837975 11484 controller_agent.cc:36] Only support MPC controller or Lat + Lon controllers as of now
+I0325 13:34:28.838163 11484 lat_controller.cc:84] Using LQR-based Lateral Controller
+I0325 13:34:28.838241 11484 lon_controller.cc:76] LON_CONTROLLER used.
+I0325 13:34:28.838587 11484 lat_controller.cc:250] Lateral control gain scheduler loaded
+I0325 13:34:28.838889 11484 lat_controller.cc:155] LQR-based Lateral Controller begin.
+I0325 13:34:28.838897 11484 lat_controller.cc:156] [LatController parameters] mass_: 2080, iz_: 4208.3, lf_: 1.4224, lr_: 1.4224
+I0325 13:34:28.838944 11484 controller_agent.cc:94] Controller init done!
+I0325 13:34:28.838956 11484 lon_controller.cc:128] Control calibration table loaded
+I0325 13:34:28.838973 11484 lon_controller.cc:129] Control calibration table size is 2694
+I0325 13:34:28.840996 11484 controller_agent.cc:94] Controller <LON_CONTROLLER> init done!
+I0325 13:34:28.850347 11484 control_component.cc:98] Control resetting vehicle state, sleeping for 1000 ms ...
+I0325 13:34:29.850587 11484 control_component.cc:104] Control default driving action is STOP
+E0325 13:34:29.852226 11491 control_component.cc:265] Chassis msg is not ready!
+I0325 13:34:29.868499 11495 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+I0325 13:34:29.874752 11493 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+I0325 13:34:29.885205 11491 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+I0325 13:34:29.896140 11501 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+I0325 13:34:29.907552 11500 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+I0325 13:34:29.917747 11486 lon_controller.cc:413] the last point found in path and speed > speed_deadzone
+...
+
+Now I'm wondering if I have missed some important configuration, or it's just unable to test the control module without a real car (with real GPS message input)?
+
+
+Not as of now, but stay tuned ! :)
+
+
+
+
+
+
+//
 
 
 
