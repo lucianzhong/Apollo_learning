30dcb4501e8bbae11bbae91fed41483fb601b71b
LucianZhong <lucianzhong@gmail.com>
2019-11-26 15:33:50 +0800

    Update Apollo_3.5_51VR_configuration.cpp

--- Apollo_3.5_51VR_configuration.cpp
+++ Apollo_3.5_51VR_configuration.cpp
@@ -17,6 +17,7 @@ ssh-add
 
 
 3. Github 加速 in Ubuntu & the same in Docker
+sudo vim /etc/hosts
    用浏览器访问 IPAddress.com 使用 IP Lookup 工具获得这个域名的ip地址，该网站可能需要梯子，输入上述域名后，分别获得github.com和github.global.ssl.fastly.net对应的ip，比如192.168.xx.xx和185.31.17.xx。准备工作做完之后，在vi打开的hosts文件中添加如下格式：
 
 192.168.xx.xx github.com
